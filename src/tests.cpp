35303edea9462f852511cb240310b806ac200c4e
Ashton Dudley <adudley@dal.ca>
2025-03-28 03:04:21 -0300

    Added basic unit tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,9 +1,13 @@
 // tests.cpp
 // Author: Ashton Dudley
-// Various unit tests
+// Various unit tests, asserts are used to enforce tests
 
+
+// Lib
 #include <iostream>
 #include <string>
+#include <cassert>  
+
 
 // Objects
 #include "Sensor.h"
@@ -12,12 +16,58 @@
 #include "ADC.h"
 
 
+// Object unit tests
+void testADCObject() {
+    if (1) {
+        ADC adc;
+        assert(adc.getResolution() == 12);
+    }
+    if (1) {
+        ADC adc(10);
+
+        assert(adc.getResolution() == 10);
+
+        adc.setResolution(8);
+
+        int adc_value = adc.read();
+        int adc_res = adc.getResolution();
+
+        std::cout << adc_value << std::endl;
+
+        assert(adc_res == 8); // assert terminates the program if not true
+    }
+
+
+    return;
+}
+
+void testSensorObject() {
+    // todo
+    return;
+}
+
+void testThrottleObject() {
+    // todo
+    return;
+}
+
+void testBrakeObject() {
+    // todo
+    return;
+}
+
+
+
+
 bool runTests(void) {
 
     std::cout << "[DEBUG] RUNNING TESTS" << std::endl;
+    testADCObject();
+    testSensorObject();
+    testThrottleObject();
+    testBrakeObject();
 
 
 
-
-    return false;
+    return true;
 }
