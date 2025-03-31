42dfdfa0cb815307f10fb4230fdf099f8966baae
Ashton Dudley <adudley@dal.ca>
2025-03-30 22:53:46 -0300

    moved tests from /test to /src

---
+++ src/tests.cpp
@@ -0,0 +1,17 @@
+// tests.cpp
+// Author: Ashton Dudley
+// Various unit tests, asserts are used to enforce tests
+// Uses assert to terminate the program if test is not true
+
+#include "tests.h"
+
+bool runTests(void) {
+
+    std::cout << "[DEBUG] Running Tests" << std::endl;
+    testADCObject();
+    testSensorObject();
+    testThrottleObject();
+    testBrakeObject();
+
+    return true;
+}
