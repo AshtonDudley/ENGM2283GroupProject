574970ba7226732d4ee65e72bcca67531a7a8ed0
Ashton Dudley <adudley@dal.ca>
2025-04-04 00:58:26 -0300

    Added switch case

--- src/menu.cpp
+++ src/menu.cpp
@@ -25,11 +25,17 @@ char printMenu() {
     char userInput = '0';
 
     cout << endl;
-    cout << "0 - add generic sensor" << endl;
-    cout << "1 - add brake pressure sensor" << endl;
-    cout << "2 - add throttle position sensor" << endl;
+    cout << "1 - store generic sensor" << endl;
+    cout << "2 - store brake pressure sensor" << endl;
+    cout << "3 - store throttle position sensor" << endl;
+    cout << "p - print the full list" << endl;
+    cout << "d - delete sensor" << endl; // sub menu --> enter seneor id
+    cout << "s - sort by sensors current value" << endl;
+    cout << "c - count total number of sensors" << endl; // say if list is empty 
+    cout << "x - clear the list" << endl; 
     
-    cout << "q - quit" << endl;
+
+    cout << "q - quit" << endl << endl;
 
     cin >> userInput;
 
@@ -38,17 +44,47 @@ char printMenu() {
 
 
 
+
 bool runMenu() {
 
     ADC adc(12, 8);  // 12-bit ADC with 8 channels
     stack<Sensor*> list;
-    int channelCounter = 0;
-    char ch;
-    string id;
 
-    while (true) {
+    char userInput = 0;
 
-        printMenu();
+    // test functions of list 
+
+
+    while (userInput != 'q') {
+
+        
+        userInput = printMenu();
+
+        switch(userInput){
+        case 0:
+            break;
+
+        case 'q':
+            break;
+        case 'p':
+            break;
+        case 'd':
+            break;
+        case 's':
+            break;
+        case 'c':
+            break;
+        case 'x':
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+        }
 
 
 
