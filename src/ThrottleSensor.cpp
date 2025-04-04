48d907619c19d146c02cb27c413913dc57dee510
Ashton Dudley <adudley@dal.ca>
2025-04-04 02:00:52 -0300

    Break sensor (#30)
    * Update BrakeSensor.h
    
    * start
    
    start
    
    * push 2
    
    * 3
    
    * commented out func
    
    * renamed file
    
    * Added function prototypes
    
    * updated throttle header and added isBreaking to breaksensor cpp
    
    * testBrake update
    
    * BrakeSensor Class Done
    
    * ThrottleSensor Class done
    
    * fixed convertToPSI() lerp
    
    * Fixing github action bug
    
    * Fixed type cast bug
    
    ---------
    
    Co-authored-by: JackParlee <jackwparlee@gmail.com>

--- src/ThrottleSensor.cpp
+++ src/ThrottleSensor.cpp
@@ -1,1 +1,38 @@
+// ThrottleSensor.cpp
+// Author: Jack Parlee
+
 #include "ThrottleSensor.h"
+using namespace std;
+
+ThrottleSensor::ThrottleSensor(ADC& adcRef, std::string n, int ch)
+	: Sensor(adcRef, n, ch), minThrottle(0), maxThrottle(0) {
+}
+
+unsigned int ThrottleSensor::read()
+{
+	return Sensor::read();
+}
+
+
+float ThrottleSensor::convertToVolt()
+{
+	float Volts;
+	int minADC = 0;
+	int maxADC = 1028;
+
+	float range = static_cast<float>(maxThrottle - minThrottle);
+	float adcRange = static_cast<float>(maxADC - minADC);
+	Volts = minThrottle + ((static_cast<float>(currentValue - minADC) * range) / adcRange);
+
+
+	return Volts;
+}
+
+void ThrottleSensor::print(ostream& out)
+{
+	Sensor::print(out);
+	out << "Min Throttle: " << minThrottle << "Max Throttle: " << maxThrottle << "Volts: ";
+	out << convertToVolt();
+	out << endl;
+}
+
--- src/testBrake.cpp
+++ src/testBrake.cpp
@@ -1,14 +1,29 @@
 // testBrake.cpp
-// Author:  
+// Author: Jack Parlee 
 
 #include "tests.h"
 
 #include "BrakeSensor.h"
+using namespace std;
 
 void testBrakeObject() {
     // todo
 
-    std::cout << "[TEST] testBrakeObject passed. \n" << std::endl;
+    if (0) {
+        ADC adc;
+        BrakeSensor brake(adc, "brake", 1);
+
+        cout << brake.getName() << endl;
+        brake.setAdcValue(150);
+        cout << brake.read() << endl;
+        cout << "Braking: " << brake.isBraking() << endl;
+        cout << "PSI: " << brake.convertToPSI() << endl;
+        brake.print(cout);
+       
+
+        //assert(brake.read() == 100);
+    }
+    cout << "[TEST] testBrakeObject passed. \n" << endl;
 
     return;
 }
